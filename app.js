2a6d102b8213b44f21be7541741537a9074e9b24
Fernando Cesena <fcesena1@yahoo.com>
2023-01-07 01:52:34 -0800

    added static route and postbank

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
+const postBank = require('./postBank');
 
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(express.static('public'));
+
+
+
+app.get("/", (req, res) => {
+  res.send(`
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <h1>Wizard News</h1>
+  </body>
+  </html>
+   `);
+});
 
 const PORT = 1337;
 
